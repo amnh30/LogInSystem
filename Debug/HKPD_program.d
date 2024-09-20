e5e63aad8064a88d4c9dae0ddae9b60b3ccb40dd
amnh30 <amn951753@gmail.com>
2024-09-20 06:46:05 +0300

    Initial commit

---
+++ Debug/HKPD_program.d
@@ -0,0 +1,15 @@
+HKPD_program.o HKPD_program.o: ../HKPD_program.c ../STD_TYPES.h \
+  ../BIT_MATH.h ../MDIO/MDIO_Interface.h ../HKPD_private.h \
+  ../HKPD_config.h ../HKPD_interface.h
+
+../STD_TYPES.h:
+
+../BIT_MATH.h:
+
+../MDIO/MDIO_Interface.h:
+
+../HKPD_private.h:
+
+../HKPD_config.h:
+
+../HKPD_interface.h:
---
+++ Debug/Main.d
@@ -0,0 +1,12 @@
+Main.o Main.o: ../Main.c ../STD_TYPES.h ../BIT_MATH.h \
+  ../MDIO/MDIO_Interface.h ../HLCD/HLCD_interface.h ../HKPD_interface.h
+
+../STD_TYPES.h:
+
+../BIT_MATH.h:
+
+../MDIO/MDIO_Interface.h:
+
+../HLCD/HLCD_interface.h:
+
+../HKPD_interface.h:
